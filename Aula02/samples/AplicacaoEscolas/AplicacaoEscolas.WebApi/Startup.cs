b5383544191fc936f2ccbd3fa858ba89aad4afb0
Gabriel Schmitt Kohlrausch <gabriel@eximia.co>
2021-11-23 10:39:05 -0300

    Exemplo criado em aula02

--- Aula02/samples/AplicacaoEscolas/AplicacaoEscolas.WebApi/Startup.cs
+++ Aula02/samples/AplicacaoEscolas/AplicacaoEscolas.WebApi/Startup.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AplicacaoEscolas.WebApi.Infraestrutura;
 
 namespace AplicacaoEscolas.WebApi
 {
@@ -26,6 +27,8 @@ namespace AplicacaoEscolas.WebApi
         {
 
             services.AddControllers();
+
+            services.AddSingleton<TurmasRepositorio>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

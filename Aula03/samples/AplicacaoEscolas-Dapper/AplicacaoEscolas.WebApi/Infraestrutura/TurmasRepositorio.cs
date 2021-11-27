7ffcbe9bd3e60528391d49f3e06aef9fa0d87c85
Gabriel Schmitt Kohlrausch <gabriel@eximia.co>
2021-11-27 07:46:54 -0300

    exemplos base aula 03 (ado, dapper, efcore)

---
+++ Aula03/samples/AplicacaoEscolas-Dapper/AplicacaoEscolas.WebApi/Infraestrutura/TurmasRepositorio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AplicacaoEscolas.WebApi.Models;
+
+namespace AplicacaoEscolas.WebApi.Infraestrutura
+{
+    public sealed class TurmasRepositorio
+    {
+        private readonly List<Turma> _turmas;
+
+        public TurmasRepositorio()
+        {
+            _turmas = new List<Turma>();
+        }
+
+        public void Inserir(Turma turma)
+        {
+            _turmas.Add(turma);
+        }
+
+        public IEnumerable<Turma> RecuperarTodos()
+        {
+            return _turmas;
+        }
+    }
+}
---
+++ Aula03/samples/AplicacaoEscolas-EFCore/AplicacaoEscolas.WebApi/Controllers/TurmasController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AplicacaoEscolas.WebApi.Infraestrutura;
+using AplicacaoEscolas.WebApi.Models;
+
+namespace AplicacaoEscolas.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurmasController : ControllerBase
+    {
+        private readonly TurmasRepositorio _turmasRepositorio;
+
+        public TurmasController(TurmasRepositorio turmasRepositorio)
+        {
+            _turmasRepositorio = turmasRepositorio;
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody]Turma turma)
+        {
+            turma.Id = Guid.NewGuid();
+            _turmasRepositorio.Inserir(turma);
+
+            return Ok(turma);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarTodos()
+        {
+
+            return Ok(_turmasRepositorio.RecuperarTodos());
+        }
+    }
+}
